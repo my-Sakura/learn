88f561cc66de57215c30ba1fd09fe3e7b739a2b8
whirlwinder <69021934+my-Sakura@users.noreply.github.com>
2021-10-11 14:56:49 +0800

    add sort && search

---
+++ algorithes/search/b_search.go
@@ -0,0 +1,26 @@
+package main
+
+// import "fmt"
+
+// func bSearch(list []int, target int) int {
+// i, j := 0, len(list)-1
+
+// for i <= j {
+// 	mid := int(uint(i+j) >> 1)
+
+// 	if target < list[mid] {
+// 		j = mid - 1
+// 	} else if target > list[mid] {
+// 		i = mid + 1
+// 	} else {
+// 		return mid
+// 	}
+// }
+
+// return -1
+// }
+
+// func main() {
+// 	s := []int{1, 2, 3, 4, 5, 6, 7}
+// 	fmt.Println(bSearch(s, 0))
+// }
